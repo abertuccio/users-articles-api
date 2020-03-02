429982cc02f37c0c12722c3e59289fc0eacf2a6c
abertuccio <abertuccio@gmail.com>
2020-03-02 15:34:54 -0300

    articles

--- validators/article.js
+++ validators/article.js
@@ -1,7 +1,28 @@
 async function articleValidation(model, article) {
 
     let validation = { errors: [], valid: false };
- 
+
+    if (!article.hasOwnProperty("userId") ||
+        !article.hasOwnProperty("title") ||
+        !!article.hasOwnProperty("text") ||
+        !article.hasOwnProperty("tags")) {
+
+        validation.errors.push(`You shoud send the request 
+                                using the format: 
+                                {
+                                    userId: String,
+                                    title: String, 
+                                    text: String, 
+                                    tags: String 
+                                }`);
+
+        validation.valid = false;
+        return validation;
+
+    }
+
+    return validation;
+
 
 }
 
